443a8232abbeec5677934b32bba97043550f8ce8
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2020-10-12 23:39:01 +0300

    First commit

---
+++ src/main/java/com/andrius/easyGift/EasyGiftApplication.java
@@ -0,0 +1,13 @@
+package com.andrius.easyGift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyGiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrius/easyGift/EasyGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrius.easyGift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
